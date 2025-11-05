6d8b2c307a74bdf02ef251683841a0ada9058a41
FR <137614590+FilipRuman@users.noreply.github.com>
2025-11-05 17:37:53 +0100

    lexer works

---
+++ astro.config.mjs
@@ -0,0 +1,42 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import { ion } from "starlight-ion-theme";
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      customCss: [
+        // Relative path to your @font-face CSS file.
+        "./src/fonts/font-face.css",
+        "./src/styles/custom.css",
+      ],
+      title: "FR",
+      plugins: [ion()],
+      expressiveCode: {
+        themes: ["tokyo-night"],
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/FilipRuman",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+
+        {
+          label: "simple parser in rust",
+          autogenerate: { directory: "writing a simple parser in rust" },
+        },
+      ],
+    }),
+  ],
+});
