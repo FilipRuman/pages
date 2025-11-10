533761bbc89704617786da4af8d5865a3cbe7e1f
FR <137614590+FilipRuman@users.noreply.github.com>
2025-11-10 13:44:51 +0100

    dbg 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,29 +3,24 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import starlightUtils from "@lorenzo_lewis/starlight-utils";
 import starlightImageZoom from 'starlight-image-zoom'
-// themes
-import { ion } from "starlight-ion-theme";
-import pagePlugin from "@pelagornis/page";
-// https://astro.build/config
 export default defineConfig({
   site: "https://filipruman.github.io",
   base: "/pages",
   integrations: [
     starlight({
-      // customCss: [
-      //   // Relative path to your @font-face CSS file.
-      //   "./src/fonts/font-face.css",
-      //   "./src/styles/custom.css",
-      // ],
+       customCss: [
+         // Relative path to your @font-face CSS file.
+         "src/fonts/font-face.css",
+         "src/styles/custom.css",
+       ],
       title: "FR",
       plugins: [
-        // ion(),
 
-        //pagePlugin(),
-        // starlightImageZoom(),
-        // starlightUtils({
-        //   multiSidebar: true,
-        // }),
+        pagePlugin(),
+         starlightImageZoom(),
+         starlightUtils({
+           multiSidebar: true,
+         }),
 
       ],
       // expressiveCode: {
